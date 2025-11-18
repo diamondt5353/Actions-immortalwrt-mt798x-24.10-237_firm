9c975f9f084f53a25139df3e087e61840ece4f70
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-11-18 17:16:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -26,8 +26,8 @@ COMPILE_DATE=$(date +"%Y年%m月%d日")
 sed -i "s/%D/\/ openwrt/g" package/base-files/files/usr/lib/os-release
 sed -i "s/%D/\/ openwrt/g" package/base-files/files/etc/openwrt_release
 
-sed -i "s/%V/\/24.10.4 /g" package/base-files/files/usr/lib/os-release
-sed -i "s/%V/\/24.10.4 /g" package/base-files/files/etc/openwrt_release
+sed -i "s/%V/\/ 24.10.4 /g" package/base-files/files/usr/lib/os-release
+sed -i "s/%V/\/ 24.10.4 /g" package/base-files/files/etc/openwrt_release
 
 sed -i "s/%C/\/ 编译日期： ${COMPILE_DATE}  by 微信:Mr___zjz/g" package/base-files/files/usr/lib/os-release  
 sed -i "s/%C/\/ 编译日期： ${COMPILE_DATE}  by 微信:Mr___zjz/g" package/base-files/files/etc/openwrt_release
